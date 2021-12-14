372c94ff534c6183bc71c1eca0f4779223ea23cc
sanggayasa <sanggayasa502@gmail.com>
2021-12-14 18:56:54 +0700

    aplikasi di chache jadi offline

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
-  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
